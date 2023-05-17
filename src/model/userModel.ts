730c198f0c39ba68a0f4bab4f7547da18ba80e13
buixuandinh711 <buixuandinh711@gmail.com>
2023-05-17 14:35:47 +0700

    add user model

---
+++ src/model/userModel.ts
@@ -0,0 +1,48 @@
+import { Schema, model, Document, Model, HydratedDocument } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface IUserMethods {
+    comparePassword: (password: string) => Promise<boolean>;
+}
+
+interface UserModel extends Model<IUser, {}, IUserMethods> {
+    createUser: (name: string, email: string, password: string) => Promise<HydratedDocument<IUser, IUserMethods>>;
+}
+
+const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+UserSchema.statics.createUser = async function (
+    name: string,
+    email: string,
+    password: string
+): Promise<HydratedDocument<IUser, IUserMethods>> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new this({ name, email, password: hashedPassword });
+    return await user.save();
+};
+
+UserSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+};
+
+export default model<IUser>("User", UserSchema);
